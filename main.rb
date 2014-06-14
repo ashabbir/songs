59e767ab9a19c3517b9ab4961e3141434069d788
Ahmed Shabbir <Shabbir10314@gmail.com>
2014-06-14 12:51:43 -0400

    init

---
+++ main.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  erb :home
+end
+
+get '/about' do
+  erb :about
+end 
+
+get '/contact' do 
+  erb :contact, :layout => :special
+end
+
