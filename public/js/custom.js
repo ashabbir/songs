4e3a56d4890ed7507a53a0d30f0cc765d1e14b68
Ahmed Shabbir <Shabbir10314@gmail.com>
2014-06-15 00:53:13 -0400

    added themes - about page - contact page need to put backend on contact

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,30 +1,33 @@
-$(document).ready(function() {   
-            var sideslider = $('[data-toggle=collapse-side]');
-            var sel = sideslider.attr('data-target');
-            var sel2 = sideslider.attr('data-target-2');
-            sideslider.click(function(event){
-                $(sel).toggleClass('in');
-                $(sel2).toggleClass('out');
-            });
+$(document).ready(function() {  
+	
+	//to set the responsive nav bar dropdown
+	var sideslider = $('[data-toggle=collapse-side]');
+	var sel = sideslider.attr('data-target');
+	var sel2 = sideslider.attr('data-target-2');
+	sideslider.click(function(event){
+		$(sel).toggleClass('in');
+		$(sel2).toggleClass('out');
+	});
 			
-			$('.navbar li').click(function(e) {
-			    $('.navbar li.active').removeClass('active');
-			    var $this = $(this);
-			    if (!$this.hasClass('active')) {
-			        $this.addClass('active');
-			    }
-			});
 			
-			$("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
-			        e.preventDefault();
-			        $(this).siblings('a.active').removeClass("active");
-			        $(this).addClass("active");
-			        var index = $(this).index();
-			        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
-			        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
-			    });
+	//to set the nav bar active depending on path
+	$('ul.nav > li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
+			
+			
+	$("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+		e.preventDefault();
+		$(this).siblings('a.active').removeClass("active");
+		$(this).addClass("active");
+		var index = $(this).index();
+		$("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+		$("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+	});
 
-
+/*	var options = {
+	    "backdrop" : "static"
+	}
+	$('#aboutModal').modal(options);
+	*/
 });
 		
 		
