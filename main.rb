c2c7cc90afd742281ff9b8ebbf1d38833c4dbb55
Ahmed Shabbir <Shabbir10314@gmail.com>
2014-06-15 19:23:55 -0400

    basic add new song added

--- main.rb
+++ main.rb
@@ -26,14 +26,38 @@ get '/contact' do
 end
 
 
+#add new  C
+get '/songs/new' do
+  @song = Song.new
+  erb :song_new
+end
+
+
+post '/songs' do
+  song = Song.create(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+
+#show list R
 get '/songs' do
   @songs = Song.all
   erb :songs
 end
 
-
+#show single R
 get '/songs/:id' do
   @song = Song.get(params[:id])
   erb :song_show
 end
+
+
+
+#delete single D
+delete '/songs/:id' do
+  @song = Song.get(params[:id]).destroy
+  redirect to('/songs')
+end
+
+
 
