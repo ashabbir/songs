38fb4b730e4a4e8ed1e4b98da9be7ee0a6f701f5
Ahmed Shabbir <Shabbir10314@gmail.com>
2014-06-15 23:03:16 -0400

    db setup

--- main.rb
+++ main.rb
@@ -8,6 +8,16 @@ set :static, true
 set :root, File.dirname(__FILE__)
 set :public, 'public'
 
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
+
+
 not_found do
   erb :not_found
 end
